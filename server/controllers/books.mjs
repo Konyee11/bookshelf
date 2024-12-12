25d5a448dfbd19567378501a92577a0d48b4ad90
Konyee11 <konyee.527@gmail.com>
2024-12-12 21:52:59 +0900

    APIルートとコントローラーを整理し、書籍の取得および削除機能を追加

--- server/controllers/books.mjs
+++ server/controllers/books.mjs
@@ -1,6 +1,17 @@
 import { validationResult } from "express-validator";
 import Book from "../models/books.mjs";
 
+async function getAllBooks(req, res) {
+    const books = await Book.find().sort({ updatedAt: -1 });
+    res.json(books);
+}
+
+async function getBookById(req, res) {
+    const _id = req.params.id;
+    const book = await Book.findById(_id);
+    res.json(book);
+}
+
 async function registBook(req, res) {
     const errors = validationResult(req); // バリデーションエラーを取得
 
@@ -36,4 +47,10 @@ async function updateBook(req, res) {
     res.json(book);
 }
 
-export { registBook, updateBook };
+async function deleteBook(req, res) {
+    const _id = req.params.id;
+    await Book.deleteOne({ _id: _id });
+    res.json({ message: "Deleted" });
+}
+
+export { getAllBooks, getBookById, registBook, updateBook, deleteBook };
