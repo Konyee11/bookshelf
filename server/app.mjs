2c27ce43b5b384d819358cc3a4aa5317908de286
Konyee11 <konyee.527@gmail.com>
2024-12-12 22:10:20 +0900

    パスが存在しない場合に404エラーを返すミドルウェアを追加

--- server/app.mjs
+++ server/app.mjs
@@ -11,6 +11,11 @@ app.use(express.json());
 
 app.use("/api", apiRoute);
 
+// パスが存在しない場合は 404 エラーを返す
+app.use((req, res) => {
+    res.status(404).json({ message: "Not Found" });
+});
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
