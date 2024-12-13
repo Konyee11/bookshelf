82fdafd327ccee88661a1dee2661cddf5f740e1c
Konyee11 <konyee.527@gmail.com>
2024-12-13 12:30:39 +0900

    HTMLの言語設定を日本語に変更し、テーマカラーを追加。BookContextのコードを整形し、可読性を向上。不要なインポートを削除。

--- src/contexts/BookContext.jsx
+++ src/contexts/BookContext.jsx
@@ -11,45 +11,46 @@ const useBooks = () => useContext(BookContext);
 const useDispatchBooks = () => useContext(BookDispatchContext);
 
 const reducer = (books, action) => {
-  switch (action.type) {
-    case "book/init":
-      return action.books;
+    switch (action.type) {
+        case "book/init":
+            return action.books;
 
-    case "book/add":
-      return [action.book, ...books];
+        case "book/add":
+            return [action.book, ...books];
 
-    case "book/delete":
-      return books.filter((_book) => _book._id !== action.book._id);
+        case "book/delete":
+            return books.filter((_book) => _book._id !== action.book._id);
 
-    case "book/update":
-      const updatedBooks = books.filter(
-        (_book) => _book._id !== action.book._id
-      );
-      updatedBooks.unshift(action.book);
-      return updatedBooks;
+        case "book/update": {
+            const updatedBooks = books.filter(
+                (_book) => _book._id !== action.book._id
+            );
+            updatedBooks.unshift(action.book);
+            return updatedBooks;
+        }
 
-    default:
-      return books;
-  }
+        default:
+            return books;
+    }
 };
 
 const BookProvider = ({ children }) => {
-  const [books, dispatch] = useReducer(reducer, []);
+    const [books, dispatch] = useReducer(reducer, []);
 
-  // 初期化
-  useEffect(() => {
-    bookApi.getAll().then((_books) => {
-      dispatch({ type: "book/init", books: _books });
-    });
-  }, []);
+    // 初期化
+    useEffect(() => {
+        bookApi.getAll().then((_books) => {
+            dispatch({ type: "book/init", books: _books });
+        });
+    }, []);
 
-  return (
-    <BookContext.Provider value={books}>
-      <BookDispatchContext.Provider value={dispatch}>
-        {children}
-      </BookDispatchContext.Provider>
-    </BookContext.Provider>
-  );
+    return (
+        <BookContext.Provider value={books}>
+            <BookDispatchContext.Provider value={dispatch}>
+                {children}
+            </BookDispatchContext.Provider>
+        </BookContext.Provider>
+    );
 };
 
 export { useBooks, useDispatchBooks, BookProvider };
